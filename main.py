874f0d696894da61ce5391fe13e41793cd702269
olehandro <odpash.itmo@gmail.com>
2023-12-01 18:09:43 +0300

    first test

--- main.py
+++ main.py
@@ -41,8 +41,7 @@ async def amo_handler(owner_id):
         print("Не удалось установить соединение с AmoCRM!")
         return 'ok'
     amo_message, contact = await amo_connection.get_last_message(user_id_hash)
-    print(amo_message, contact, message)
-    if contact == 'user' or amo_message != message:
+    if contact == 'user':
         print("Сообщение уже распознавалось")
         return 'ok'
     api.add_message(message=message, lead_id=lead_id, is_bot=False)
